351f0b85d1881b0d2b31743647e103371804eaea
anmatngu <nxan2911@gmail.com>
2024-12-08 20:19:15 +0700

    Update project data and custom MDX examples with new projects and enhanced content on AI-driven data augmentation

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,24 +7,24 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Project One",
+    title: "REColor",
+    year: 2024,
+    description:
+      "An AI-driven application for recoloring grayscale and faded images, enhancing visual quality with precision and a user-friendly interface.",
+    url: "https://github.com/lunovian/REColor",
+  },
+  {
+    title: "AI Tool for Room Decoration",
+    year: 2024,
+    description:
+      "An AI-powered assistant that provides tailored recommendations for furniture, color schemes, and room arrangements based on preferences and layout.",
+    url: "https://github.com/Ekanara/AI-Tool-for-Room-Decoration",
+  },
+  {
+    title: "ANAug",
     year: 2023,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
-  {
-    title: "Project Two",
-    year: 2022,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
-  {
-    title: "Project Three",
-    year: 2021,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
+      "A Python-based data augmentation library for specialized domains like medical imaging, improving model robustness and performance in niche applications.",
+    url: "https://github.com/lunovian/an-augment",
   },
 ];
