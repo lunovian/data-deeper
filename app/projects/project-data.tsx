0b41fd8e397c5e04ae9499802fa1844a0a3c18c5
anmatngu <nxan2911@gmail.com>
2024-12-31 03:04:25 +0700

    Update project-data.tsx

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,24 +7,24 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "REColor",
+    title: "ChromaFusion",
     year: 2024,
     description:
       "An AI-driven application for recoloring grayscale and faded images, enhancing visual quality with precision and a user-friendly interface.",
-    url: "https://github.com/lunovian/REColor",
-  },
-  {
-    title: "AI Tool for Room Decoration",
-    year: 2024,
-    description:
-      "An AI-powered assistant that provides tailored recommendations for furniture, color schemes, and room arrangements based on preferences and layout.",
-    url: "https://github.com/Ekanara/AI-Tool-for-Room-Decoration",
+    url: "https://github.com/lunovian/ChromaFusion",
   },
   {
     title: "ANAug",
-    year: 2023,
+    year: 2024,
     description:
       "A Python-based data augmentation library for specialized domains like medical imaging, improving model robustness and performance in niche applications.",
     url: "https://github.com/lunovian/an-augment",
+  },
+  {
+    title: "AI Tool for Room Decoration",
+    year: 2023,
+    description:
+      "An AI-powered assistant that provides tailored recommendations for furniture, color schemes, and room arrangements based on preferences and layout.",
+    url: "https://github.com/Ekanara/AI-Tool-for-Room-Decoration",
   },
 ];
